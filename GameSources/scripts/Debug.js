597a79f0dfa0f4a13b6cfd14eb48a99a69152d71
Junyoung Park <ssam1221@naver.com>
2022-02-02 22:37:51 +0900

    Add debug log types

--- GameSources/scripts/Debug.js
+++ GameSources/scripts/Debug.js
@@ -1,3 +1,5 @@
+let debugMode = `debug`;
+
 export default class Debug {
 
     constructor({
@@ -6,14 +8,34 @@ export default class Debug {
         this.filename = filename;
     }
 
+    static setDebugMode(_debugMode) {
+        console.log(`Set debug mode to : ${_debugMode}`)
+        debugMode = _debugMode;
+    }
+
     getFormattedDate() {
         const now = new Date();
         return `[${now.getFullYear()}/${(`00` + (now.getMonth()+1)).substr(-2)}/${(`00` + now.getDate()).substr(-2)} ` +
             `${(`00` + (now.getHours()+1)).substr(-2)}:${(`00` + (now.getMinutes()+1)).substr(-2)}:${(`00` + (now.getSeconds()+1)).substr(-2)}]`;
     }
 
+    info(...str) {
+        const _filename = `                         ${this.filename}`.substr(-20);
+        console.info(`[ INFO]${this.getFormattedDate()}[${_filename}] `, ...str)
+    }
+
+    warn(...str) {
+        const _filename = `                         ${this.filename}`.substr(-20);
+        console.warn(`[ WARN]${this.getFormattedDate()}[${_filename}] `, ...str)
+    }
+
+    error(...str) {
+        const _filename = `                         ${this.filename}`.substr(-20);
+        console.error(`[ERROR]${this.getFormattedDate()}[${_filename}] `, ...str)
+    }
+
     log(...str) {
         const _filename = `                         ${this.filename}`.substr(-20);
-        console.log(`${this.getFormattedDate()}[${_filename}] `, ...str)
+        console.log(`[  LOG]${this.getFormattedDate()}[${_filename}] `, ...str)
     }
 }
